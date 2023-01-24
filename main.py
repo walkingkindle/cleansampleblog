2756af475f9bf942a3d15b8ed9510af3c1fb845b
walkingkindle <122030667+walkingkindle@users.noreply.github.com>
2023-01-24 14:27:40 +0800

    Updating the contact button and having it send the specified message to the mail

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import flask
 from flask import Flask
 import requests
+from flask import request
+import smtplib
+
 
 
 app = Flask(__name__)
@@ -22,6 +25,27 @@ def show_post(index):
     url = "https://api.npoint.io/93f14c94fb7ca980d04a"
     response = requests.get(url=url).json()
     return flask.render_template('post.html',response=response,index=int(index))
+
+@app.route('/form-entry',methods = ['POST','GET'])
+def receive_data():
+    if request.method == "POST":
+        username = request.form['username']
+        email = request.form['email-address']
+        phone = request.form['phone-number']
+        message = request.form['message']
+        user = [username, email, phone, message]
+        print(user)
+        my_email = "YOUR EMAIL HERE"
+        my_password = "YOUR PASSWORD HERE"
+        connection = smtplib.SMTP("smtp.gmail.com",timeout=120,port=587)
+        connection.starttls()
+        connection.login(user=my_email,password=my_password)
+        connection.sendmail(from_addr=my_email,to_addrs=email,msg=message)
+        connection.close()
+        return flask.render_template('contact.html',msg_sent=True)
+    return flask.render_template('contact.html',msg_sent=False)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
